f0d26381bdff4c5222e907040dd3ca640c10c0d0
Arahmano <yayaadjagarailsadio@gmail.com>
2020-02-12 19:20:43 +0000

    commit one

---
+++ src/app/tab2/tab2.page.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-tab2',
+  templateUrl: 'tab2.page.html',
+  styleUrls: ['tab2.page.scss']
+})
+export class Tab2Page {
+  intervenant: any;
+  public donnees = [];
+  link_url = "http://app.joinpuzzle.com/public/uploads/images/intervenant/";
+  api = "http://app.joinpuzzle.com/public/api/intervenants.json";
+
+  constructor(public http: HttpClient) {
+    console.log('Ici la connexion à l\'api tous passe nickel');
+    this.getIntervenant()
+  }
+  // getIntervenant() {
+  //   return new Promise(resolve => {
+  //     this.http.get(this.api).subscribe(data => {
+  //       resolve(data);
+  //       this.donnees = data
+  //       console.log(this.donnees);
+  //     }, err => {
+  //       console.log(err);
+  //     });
+  //   });
+  // }
+
+  getIntervenant() {
+    setTimeout(() => {
+      return new Promise(resolve => {
+        this.http.get(this.api).subscribe(data => {
+          resolve(data);
+          this.donnees = data
+          console.log(this.donnees);
+        }, err => {
+          console.log(err);
+        });
+      });
+    }, 5000);
+  }
+}
